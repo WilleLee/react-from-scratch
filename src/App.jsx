1c049396cf70ce29bbb1ef3d9b1360e28cccb6cf
Wille Lee <1992season@gmail.com>
2026-07-07 01:18:36 +0900

    transfers props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import Button from "./components/Button.jsx";
 
+const INIT_NUMBER = 0;
+
+const useIncreaseNumber = () => {
+  const [count, setCount] = useState(INIT_NUMBER);
+
+  function handleIncreaseClick() {
+    setCount((prev) => prev + 1);
+  }
+
+  function handleResetClick() {
+    setCount(INIT_NUMBER);
+  }
+
+  return {
+    count,
+    handleIncreaseClick,
+    handleResetClick,
+  };
+};
+
 function App() {
+  const { count, handleIncreaseClick, handleResetClick } = useIncreaseNumber();
   return (
     <div>
       <h1>Hello React with JSX!</h1>
-      <Button>첫 번째 버튼</Button>
-      <Button>두 번째 버튼</Button>
+      <p>클릭 횟수: {count}</p>
+      <Button onClick={handleIncreaseClick}>증가</Button>
+      <Button onClick={handleResetClick}>초기화</Button>
     </div>
   );
 }
