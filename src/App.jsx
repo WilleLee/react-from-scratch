36f1e48fe740ba17dd2bf1b198cfd920ab44b256
Wille Lee <1992season@gmail.com>
2026-07-07 01:10:17 +0900

    component (Button)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Button from "./components/Button.jsx";
+
 function App() {
-  return <h1>Hello React with JSX!</h1>;
+  return (
+    <div>
+      <h1>Hello React with JSX!</h1>
+      <Button>첫 번째 버튼</Button>
+      <Button>두 번째 버튼</Button>
+    </div>
+  );
 }
 
 export default App;
