2ed59d0b4fef96d6efc8684ba7b4a4bbc5ceb2d9
Wille Lee <1992season@gmail.com>
2026-07-07 01:04:40 +0900

    src directory and App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return <h1>Hello React with JSX!</h1>;
-}
+import App from "./App.jsx";
 
 const rootElement = document.getElementById("root");
 
